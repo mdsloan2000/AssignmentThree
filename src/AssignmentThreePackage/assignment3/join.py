c92cf88f674361bc39f5405a2fc18bcd5e0737c1
Michael Sloan <mdsloan64@gmail.com>
2015-10-14 22:57:08 -0400

    Initial Setup
    All initial files installed.

---
+++ src/AssignmentThreePackage/assignment3/join.py
@@ -0,0 +1,39 @@
+import MapReduce
+import sys
+
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    key = record[1]
+    value = record
+#    lineitem = value.split()
+#    key = record[1]
+#    value = record[1]
+          
+    #words = value.split()
+    #print (key, value)
+    #for w in words:
+    mr.emit_intermediate(key, value)
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of occurrence counts
+    #for v in list_of_values:
+        #mr.emit((key, list_of_values))
+    #print key
+    print list_of_values
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
---
+++ src/AssignmentThreePackage/inverted_index.py
@@ -0,0 +1,6 @@
+'''
+Created on Oct 14, 2015
+
+@author: mdsloan2000
+'''
+print "hello world"
